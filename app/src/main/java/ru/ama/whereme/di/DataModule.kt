0b933400bd0f66d7b410b756d1aed0f7fb1616c4
10ama_pc <10ama@mail.ru>
2022-05-22 16:55:45 +0500

    add dagger2

---
+++ app/src/main/java/ru/ama/whereme/di/DataModule.kt
@@ -0,0 +1,12 @@
+package ru.ama.whereme.diO
+
+import android.app.Application
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+
+@Module
+interface DataModule {
+/*
+    */
+}
---
+++ app/src/main/java/ru/ama/whereme/di/ViewModelModule.kt
@@ -0,0 +1,13 @@
+package ru.ama.ottest.di
+
+import androidx.lifecycle.ViewModel
+
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+interface ViewModelModule {
+/*
+ */
+}
