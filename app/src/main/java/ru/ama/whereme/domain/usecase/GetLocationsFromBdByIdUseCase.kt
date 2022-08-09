1b9d8bd3940a462845ce2479c0440f81395f35f3
10ama <4321>
2022-08-09 13:59:50 +0500

    09.08.2022 add menu with days for choose

---
+++ app/src/main/java/ru/ama/whereme/domain/usecase/GetLocationsFromBdByIdUseCase.kt
@@ -0,0 +1,11 @@
+package ru.ama.whereme.domain.usecase
+
+import ru.ama.whereme.domain.repository.WmRepository
+import javax.inject.Inject
+
+class GetLocationsFromBdByIdUseCase @Inject constructor(
+    private val repository: WmRepository
+) {
+
+    operator  suspend fun invoke(mDate:String) = repository.getLocationById(mDate)
+}
